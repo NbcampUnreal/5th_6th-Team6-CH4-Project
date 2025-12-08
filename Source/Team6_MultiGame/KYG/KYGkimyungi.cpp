df2582fd0a26ad374ff30d7151948196747f2e12
Bzart148 <dbsrlqowl@gmail.com>
2025-12-08 21:11:44 +0900

    feat 테스트

---
+++ Source/Team6_MultiGame/KYG/KYGkimyungi.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KYG/KYGkimyungi.h"
+
+KYGkimyungi::KYGkimyungi()
+{
+}
+
+KYGkimyungi::~KYGkimyungi()
+{
+}
