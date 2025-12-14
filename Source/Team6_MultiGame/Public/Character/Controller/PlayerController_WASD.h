72de0d3bbbd1e3daae68229908d1d03095fb47ca
ckxlzlaoj <95674725+ckxlzlaoj@users.noreply.github.com>
2025-12-15 03:53:33 +0900

    1

---
+++ Source/Team6_MultiGame/Public/Character/Controller/PlayerController_WASD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerController_WASD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM6_MULTIGAME_API APlayerController_WASD : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Team6_MultiGame/Public/Character/PlayerController_Mouse.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerController_Mouse.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM6_MULTIGAME_API APlayerController_Mouse : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
