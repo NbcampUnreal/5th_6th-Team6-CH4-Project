aa7c2d32f3d4bac3ffeeaf3dd82880a5125f83a8
OSJK\sau03 <sau03095@naver.com>
2025-12-29 15:48:11 +0900

    온라인 서브 시스템 및 보이스챗 모듈 활성화

--- Source/Team6_MultiGame/Team6_MultiGame.Build.cs
+++ Source/Team6_MultiGame/Team6_MultiGame.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+
 
 using UnrealBuildTool;
 using UnrealBuildTool.Rules;
@@ -18,19 +18,17 @@ public class Team6_MultiGame : ModuleRules
             "NavigationSystem",
             "AIModule",
             "GameplayTasks",
-            "UMG"
+            "UMG",
+
+            "OnlineSubsystem",
+            "OnlineSubsystemEOS",
+            "OnlineSubsystemUtils",
+
+            "VoiceChat"
         });
 
         PrivateDependencyModuleNames.AddRange(new string[] { });
 
         PublicIncludePaths.AddRange(new string[] { "Team6_MultiGame" });
-
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
     }
 }
