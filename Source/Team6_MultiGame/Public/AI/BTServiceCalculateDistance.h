15b87300f793a3125a5bf4819b1781635639e196
chanyoo0916 <yc12241@gmail.com>
2025-12-30 21:30:29 +0900

    feet : AI 비헤이비어트리 공격로직 수정
    어택 쿨다운 추가, Calculate Distance 추가

---
+++ Source/Team6_MultiGame/Public/AI/BTServiceCalculateDistance.h
@@ -0,0 +1,16 @@
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTServiceCalculateDistance.generated.h"
+
+UCLASS()
+class TEAM6_MULTIGAME_API UBTServiceCalculateDistance : public UBTService
+{
+    GENERATED_BODY()
+
+public:
+    UBTServiceCalculateDistance();
+
+protected:
+    // 주기적으로 실행될 함수
+    virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
